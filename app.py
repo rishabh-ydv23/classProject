dc5fd477519ba81755c79501304cbb98e5811969
Rishabh <rishabhyadavunnao@gmail.com>
2026-07-29 15:45:28 +0530

    Improve code quality and UI

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+# Home Page (UI)
 @app.route("/", methods=["GET", "POST"])
 def home():
     result = None
@@ -17,6 +19,19 @@ def home():
             result = num1 - num2
 
     return render_template("index.html", result=result)
+
+
+# API Route for Addition
+@app.route("/add/<int:a>/<int:b>")
+def add(a, b):
+    return str(a + b)
+
+
+# API Route for Subtraction
+@app.route("/subtract/<int:a>/<int:b>")
+def subtract(a, b):
+    return str(a - b)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
